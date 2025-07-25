f981a4b35a8cb2e8d855b816742318268bd5cf70
PraharshaReddy1 <praharsha711@gmail.com>
2025-07-25 20:29:54 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,12 @@
 
+# fibonacci.py
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=' ')
+        a, b = b, a + b
+
+if __name__ == "__main__":
+    terms = int(input("Enter number of terms: "))
+    fibonacci(terms)
